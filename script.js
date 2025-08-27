6f86944fca4643993afe272600c904b27cf6855d
Marufa Akter <marufaakter1947@gmail.com>
2025-08-27 13:17:48 +0600

    added call button alert message and coin functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// heart icon functionality
 let totalHeart=0;
 const totalClickHeart=document.getElementById("total-heart-count");
 const iconBtns=document.querySelectorAll(".heart-icon");
@@ -8,3 +9,27 @@ for(let iconBtn of iconBtns){
         totalClickHeart.innerText=totalHeart;
     })
 }
+
+// function to set inner text
+function setInnerText(value) {
+  const availableCoinsElement = document.getElementById("available-coins");
+  availableCoinsElement.innerText = value;
+}
+
+// call button functionality
+let reduceCoins=20;
+document.getElementById("national-call").addEventListener("click",function(){
+   const serviceName= document.getElementById("national-title").innerText;
+   const serviceNumber=document.getElementById("national-number").innerText;
+   const alertMessage= "📞 calling"+ " " + serviceName + " " + serviceNumber +"...";
+   alert(alertMessage);
+   const availableCoins=document.getElementById("available-coins").innerText;
+   if(availableCoins<reduceCoins){
+alert("You don't have sufficient coins");
+return;
+   }
+   const currentCoins=Number(availableCoins)- Number(reduceCoins);
+
+   setInnerText(currentCoins);
+
+})
